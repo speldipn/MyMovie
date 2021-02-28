81311177e2925c0c18fe56b0b598e930e659913e
Neo <speldipn@gmail.com>
2021-03-01 01:18:15 +0900

    Add gms library and permission about location

--- app/src/main/java/com/example/mymovie/MapActivity.kt
+++ app/src/main/java/com/example/mymovie/MapActivity.kt
@@ -7,5 +7,10 @@ class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        setup()
+    }
+
+    private fun setup() {
+
     }
 }
